9ca1d114eb542a3e828d080f44d5ee70ddf37aae
Sergey <perlinsergey@gmail.com>
2025-03-20 20:12:16 +0300

    Remove build files, IDE settings, and other unnecessary files

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sergejperlin/CLionProjects/ownRedis/tests
-# Build directory: /Users/sergejperlin/CLionProjects/ownRedis/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AVLTreeTest "/Users/sergejperlin/CLionProjects/ownRedis/build/tests/tests")
-set_tests_properties(AVLTreeTest PROPERTIES  _BACKTRACE_TRIPLES "/Users/sergejperlin/CLionProjects/ownRedis/tests/CMakeLists.txt;11;add_test;/Users/sergejperlin/CLionProjects/ownRedis/tests/CMakeLists.txt;0;")
